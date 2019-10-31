e74b7e245bbfaa32b139fd2ae90129aabd200909
Ilaria Negri <ilaria.negri@webinterpret.com>
2019-10-31 16:02:31 +0100

    added gems and stars

--- game/items/gem.py
+++ game/items/gem.py
@@ -9,26 +9,3 @@ class Gem(MapObject):
         print("I've found a gem")
         super().on_hit()
 
-
-
-
-
-
-
-"""
-
-
-class Animal:
-    def __init__(self, size, health, name):
-        self.size = size
-        self.health = health
-        self.name = name
-
-
-
-class SmallLion(Animal):
-    def __init__(self, name):
-        super().__init___("small", 10, name)
-
-lion = Lion("Sylwia")
-"""
