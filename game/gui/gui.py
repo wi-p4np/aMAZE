4e24a66098b110d4e83fbc79a4e3f8c672a9a296
Myriam López <myriam.pianista@gmail.com>
2019-11-14 16:09:21 +0100

    Modified a bit the coin score label :)

--- game/gui/gui.py
+++ game/gui/gui.py
@@ -9,14 +9,11 @@ class MyGui:
     def __init__(self):
         self.health_bar = HealthBar()
         self.gem_score = ScoreLabel('gem', 50, 600)
-        self.coins_score = ScoreLabel('coin', 150, 600)
-        self.score_label = CoinsLabel('coin2', 250, 600)
+        self.score_label = CoinsLabel('coin', 150, 600)
 
     def draw(self):
-        self.coins_score.set_text(ScoreManager.score)
         self.health_bar.draw()
         self.gem_score.draw()
-        self.coins_score.draw()
         self.score_label.draw()
 
     def update(self):
