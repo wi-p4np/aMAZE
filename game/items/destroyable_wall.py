dc03cd24a6b64aba72a77db5c9cf9bd8660313ce
Elena Greci <elena.greci@webinterpret.com>
2019-11-28 16:33:02 +0100

    The destroyable wall changes colour according to its health

--- game/items/destroyable_wall.py
+++ game/items/destroyable_wall.py
@@ -10,6 +10,7 @@ class DestroyableWall(MapObject):
         super().__init__(asset_path, scale, x, y, properties)
         self.health = 3
         self.timer = 0
+        #self.color = None
 
     def on_hit(self):
         if self.timer == 0:
@@ -18,6 +19,17 @@ class DestroyableWall(MapObject):
                 self.health -= 1
             else:
                 self.remove_from_sprite_lists()
+        self.update_colour()
+
+
+    def update_colour(self):
+        if self.health == 2:
+            self.color: RGB = (20, 50, 50) #dark orange
+        elif self.health == 1:
+            self.color: RGB = (50, 200, 200) #yellow green
+
+
 
     def update(self, delta_time):
         self.timer = max(self.timer - delta_time, 0)
+        #self.update_colour()
